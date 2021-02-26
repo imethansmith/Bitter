cf2a953a31b87da707a246f20260a92788561764
Ethan Smith <edhsmith@icloud.com>
2021-02-26 21:31:47 +1300

    Updated UI to proper auto layout dimensions. Updated picker with delegate and data loading from CoinManager.

--- Bitter/Controller/ViewController.swift
+++ Bitter/Controller/ViewController.swift
@@ -11,11 +11,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     let coinManager = CoinManager()
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 5
+        return 1
     }
+    
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
     }
     
     @IBOutlet weak var currentCurrency: UILabel!
